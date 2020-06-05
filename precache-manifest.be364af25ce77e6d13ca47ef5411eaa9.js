95fa73a725896709b88335117b5ca1dec29c4364
Milad <mosaddegh.milad@gmail.com>
2020-06-05 19:06:41 +0200

    Updates

---
+++ precache-manifest.be364af25ce77e6d13ca47ef5411eaa9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6c4f6fcf8a3965f43a5875515049de",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "cf0cf878ac61b0800f1f",
+    "url": "/My-Portfolio/static/css/2.b7f16634.chunk.css"
+  },
+  {
+    "revision": "9272472128b8fcdef791",
+    "url": "/My-Portfolio/static/css/main.b17ca7b5.chunk.css"
+  },
+  {
+    "revision": "cf0cf878ac61b0800f1f",
+    "url": "/My-Portfolio/static/js/2.660b6caf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/My-Portfolio/static/js/2.660b6caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9272472128b8fcdef791",
+    "url": "/My-Portfolio/static/js/main.7f18f5a7.chunk.js"
+  },
+  {
+    "revision": "8aba1767fb34ef398da8",
+    "url": "/My-Portfolio/static/js/runtime-main.82805dd7.js"
+  },
+  {
+    "revision": "85e7b7149a79f67744c6bef8d983c03d",
+    "url": "/My-Portfolio/static/media/B1.85e7b714.pdf"
+  },
+  {
+    "revision": "373f80aae2c5854dae6173f3d9f474bb",
+    "url": "/My-Portfolio/static/media/B2.373f80aa.pdf"
+  },
+  {
+    "revision": "c0e7f4ff6a2bd24f30076967437c2d67",
+    "url": "/My-Portfolio/static/media/Lebenslauf.c0e7f4ff.pdf"
+  },
+  {
+    "revision": "e0f5b19623784316734fa24e0863bcd5",
+    "url": "/My-Portfolio/static/media/contact-bg-2.e0f5b196.jpg"
+  },
+  {
+    "revision": "3a688ecf4528f37f0745f37d4253d8a1",
+    "url": "/My-Portfolio/static/media/cv-pdf.3a688ecf.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/My-Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/My-Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/My-Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/My-Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/My-Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "72566c18c9fb1702e2ffc91548ee4f52",
+    "url": "/My-Portfolio/static/media/it-background.72566c18.jpeg"
+  },
+  {
+    "revision": "6da204195d3826b6d8226177e663ca95",
+    "url": "/My-Portfolio/static/media/milad-1.6da20419.JPG"
+  },
+  {
+    "revision": "0d9c7130c50a8f6d4a16f996f6593a85",
+    "url": "/My-Portfolio/static/media/pic1.0d9c7130.JPG"
+  },
+  {
+    "revision": "838b4d7d1566b12df5e7fae2d227e425",
+    "url": "/My-Portfolio/static/media/telc.838b4d7d.png"
+  },
+  {
+    "revision": "4e828ba44207b9792e492456712577ce",
+    "url": "/My-Portfolio/static/media/underStructure.4e828ba4.pdf"
+  }
+]);
